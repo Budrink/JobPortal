1d4c2c4a5058c392be359a73a5a1f8c1cc70ba2b
konstantin <kbudrin@yandex.ru>
2020-08-11 16:56:07 +0500

    fix

--- Controllers/FreelancerController.cs
+++ Controllers/FreelancerController.cs
@@ -134,21 +134,33 @@ namespace JobPortal.Controllers
 			public int pageNumber { get; set; }
 			public int amountItemsOnPage { get; set; }
 		}
-		[HttpPost, Route("/feedbacks")]
+		[HttpPost, Route("feedbacks")]
 		public async Task<IActionResult> GetFeedbackList( [FromQuery] queryFeedbackDTO query )
 		{
 			try
 			{
 				var user = await _userManager.FindByIdAsync(query.freelancerId);
-				IEnumerable<Feedback> feedbackList;
+				IEnumerable<FeedbackResponseModel> feedbackList;
 				if (query.pageNumber > 0)
 				{
-					 feedbackList = user.Freelancer.Feedbacks.Skip((query.pageNumber - 1) * query.amountItemsOnPage)
+					 feedbackList = user.Freelancer.Feedbacks.Select(x=> new FeedbackResponseModel
+					 {
+						 FeedbackId = x.FeedbackId,
+						 FreelancerId = x.Freelancer.User.Id,
+						 Text = x.Text,
+						 Mark = x.Mark
+					 }).Skip((query.pageNumber - 1) * query.amountItemsOnPage)
 					  .Take(query.amountItemsOnPage).ToList();
 				}
 				else
 				{
-					feedbackList = user.Freelancer.Feedbacks.ToList();
+					feedbackList = user.Freelancer.Feedbacks.Select(x => new FeedbackResponseModel
+					{
+						FeedbackId = x.FeedbackId,
+						FreelancerId = x.Freelancer.User.Id,
+						Text = x.Text,
+						Mark = x.Mark
+					}).ToList();
 				}
 
 				return Ok(new {
