5befbeb36e0c36533e44312397d0023344d124b3
Galina <Galinab222@hotmail.com>
2020-08-21 11:34:57 +0500

    ggg

--- ClientApp/src/Components/PostData/PostFeedback.tsx
+++ ClientApp/src/Components/PostData/PostFeedback.tsx
@@ -1,28 +1,29 @@
-// import { http } from '../Data/Http';
-// export const PostFeedback = async (
-//   projectId: string, //iD of porject for feedback
-//   text: string,
-//   rating: number,
-// ): Promise<any> => {
-//   let response;
-//   let requestBody = {
-//     projectId: projectId,
-//     text: text,
-//     rating: rating,
-//   };
+import { http } from '../Data/Http';
+export const PostFeedback = async (
+  projectId: string, //iD of porject for feedback
+  text: string,
+  rating: number,
+  userId: string,
+): Promise<any> => {
+  let response;
+  let requestBody = {
+    projectId: projectId,
+    text: text,
+    rating: rating,
+  };
 
-//   try {
-//     response = await http({
-//       path: `User/delete`,
-//       method: 'POST',
-//       body: requestBody,
-//     });
-//     let r = response.parsedBody;
-//     //  if( r=true)
-//     // return true;
-//     return r;
-//   } catch (error) {
-//     window.alert(error);
-//     return false;
-//   }
-// };
+  try {
+    response = await http({
+      path: `Freelancer/${userId}/feedback`,
+      method: 'POST',
+      body: requestBody,
+    });
+    let r = response.parsedBody;
+    //  if( r=true)
+    // return true;
+    return r;
+  } catch (error) {
+    window.alert(error);
+    return false;
+  }
+};
